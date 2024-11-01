21048e44b1af073b9011c0f6bc5fc1cd6325b747
luistheneonwizard <luisjofaba@gmail.com>
2024-11-01 00:45:08 +0000

    oct 31 cambios

--- dataframepandas2.py
+++ dataframepandas2.py
@@ -4,9 +4,17 @@ df = pd.read_csv("poblacion.csv")
 
 #filtro = df[df['Date'] > 2010]
 #print(filtro[["COL", "ARG"]])
-print(df["COL"])
-df.at[0, "COL"] = 5000000
-print(df["COL"])
+
+# print(df["COL"])
+# df.at[0, "COL"] = 5000000
+# print(df["COL"])
+
+df = pd.read_csv("poblacion.csv")
+print(df)
+df = df.drop("ABW", axis=1)
+print(df)
+df.to_csv("salida.csv", index=False)
+
 # print(df.head())
 # print(df.describe())
 # print(df.tail())
